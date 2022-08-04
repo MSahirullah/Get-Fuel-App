1b77a28da41e466ff6778b9d11fdd1c2ef9c352b
MSAHIRULLAH <2019T00465@stu.cmb.ac.lk>
2022-08-04 15:40:32 +0530

    Google Map Updated

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,27 +1,110 @@
+// import 'package:flutter/material.dart';
+// import 'package:get_fuel_app/widget/custom_search_field.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// class Map extends StatefulWidget {
+//   const Map({Key? key}) : super(key: key);
+
+//   @override
+//   State<Map> createState() => _MapState();
+// }
+
+// class _MapState extends State<Map> {
+
+//   late GoogleMapController mapController;
+//   final LatLng _center = const LatLng(45.521563, -122.677433);
+
+//     void _onMapCreated(GoogleMapController controller) {
+//     mapController = controller;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SizedBox(
+//         height: 200,
+//         child: Container(
+//           padding: const EdgeInsets.all(30.0),
+//           child: Column(
+//             children: const [
+//               SizedBox(
+//                 height: 15,
+//               ),
+//               CustomSearchField(),
+//               SizedBox(
+//                 height: 25,
+//               ),
+
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:get_fuel_app/widget/custom_search_field.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Map extends StatelessWidget {
+class Map extends StatefulWidget {
   const Map({Key? key}) : super(key: key);
+  @override
+  State<Map> createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  late GoogleMapController mapController;
+
+  final LatLng showLocation = const LatLng(7.2527, 80.4594);
+  Set<Marker> markers = Set(); //markers for google map
+
+  void initState() {
+    markers.add(Marker(
+      //add marker on google map
+      markerId: MarkerId(showLocation.toString()),
+      position: showLocation, //position of marker
+      infoWindow: InfoWindow(
+        //popup info
+        title: 'My Custom Title ',
+        snippet: 'My Custom Subtitle',
+      ),
+      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
+    ));
+
+    //you can add more markers here
+    super.initState();
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: 200,
-        child: Container(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              children: const [
-                SizedBox(
-                  height: 15,
+      body: Stack(
+        children: [
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(bottom: 50),
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                markers: markers,
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                  target: showLocation,
+                  zoom: 15.0,
                 ),
-                CustomSearchField(),
-                SizedBox(
-                  height: 25,
-                ),
-              ],
-            )),
+              ),
+            ),
+          ),
+          Positioned(
+              top: 50,
+              left: 20, //0 is starting at left, use it to give left-margin
+              right: 20, //0 is ending at right, use it to give right-margin
+              child: Container(child: CustomSearchField()))
+        ],
       ),
     );
   }
--- lib/widget/custom_search_field.dart
+++ lib/widget/custom_search_field.dart
@@ -9,41 +9,44 @@ class CustomSearchField extends StatelessWidget {
     return Material(
       elevation: 2.0,
       borderRadius: BorderRadius.circular(25.0),
-      child: TextFormField(
-        key: const ValueKey("search"),
-        style: const TextStyle(
-          fontSize: 18.0,
-        ),
-        textCapitalization: TextCapitalization.sentences,
-        decoration: InputDecoration(
-          prefixIcon: const Icon(Icons.search),
-          label: const Center(
-            child: Text("Search"),
-          ),
-          contentPadding: const EdgeInsets.all(18.0),
-          labelStyle: const TextStyle(
-            color: inputFieldTextColor,
+      child: Container(
+        child: TextFormField(
+          
+          key: const ValueKey("search"),
+          style: const TextStyle(
             fontSize: 18.0,
           ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: inputFieldBorderColor),
-            borderRadius: BorderRadius.circular(25.0),
-          ),
-          disabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: inputFieldBorderColor),
-            borderRadius: BorderRadius.circular(25.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: const BorderSide(color: inputFieldBorderColor),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: const BorderSide(color: inputFieldBorderColor),
-          ),
-          focusedErrorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: const BorderSide(color: inputFieldBorderColor),
+          textCapitalization: TextCapitalization.sentences,
+          decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.search),
+            label: const Center(
+              child: Text("Search"),
+            ),
+            contentPadding: const EdgeInsets.all(18.0),
+            labelStyle: const TextStyle(
+              color: inputFieldTextColor,
+              fontSize: 18.0,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: inputFieldBorderColor),
+              borderRadius: BorderRadius.circular(25.0),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: inputFieldBorderColor),
+              borderRadius: BorderRadius.circular(25.0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: const BorderSide(color: inputFieldBorderColor),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: const BorderSide(color: inputFieldBorderColor),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: const BorderSide(color: inputFieldBorderColor),
+            ),
           ),
         ),
       ),
